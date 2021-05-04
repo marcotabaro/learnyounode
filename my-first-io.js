d3d7cba23a8758a8bb9203bfc6a55e2502e4fe62
marcotabaro <g.marcotaba@gmail.com>
2021-05-04 17:18:55 +0200

    refactoring

--- my-first-io.js
+++ my-first-io.js
@@ -1,4 +1,4 @@
 const fs = require('fs');
 
-let newlines = fs.readFileSync(process.argv[2]).toString().split('\n');
-console.log(newlines.length-1);
+let newlines = fs.readFileSync(process.argv[2]).toString().split('\n').length-1;
+console.log(newlines);
