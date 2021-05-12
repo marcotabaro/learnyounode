855bdbd8d27d7cca6dd11dfe5dd67e3f4d1528f6
marco <g.marcotaba@gmail.com>
2021-05-12 12:11:48 +0200

    fs.readdir

---
+++ filtered-ls.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+const fs = require('fs'); 
+
+const dir = process.argv[2];
+const ext = process.argv[3];
+
+//console.log(dir);
+//console.log(ext);
+
+
+
--- my-first-async-io.js
+++ my-first-async-io.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 
-let file = process.argv[2];
+const file = process.argv[2];
 
 fs.readFile(file, 'utf-8' , (err, data) => {
     if (err) {
